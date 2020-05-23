9bd96800e2f7c99082593b4fbcfc6eb1fe5ab032
silvioantonio <oliveira_0000@hotmail.com>
2020-05-23 17:43:38 -0300

    Delete condutor component

--- src/app/views/condutor/delete/delete.component.ts
+++ src/app/views/condutor/delete/delete.component.ts
@@ -1,4 +1,15 @@
+import { Condutor } from './../../../components/condutor/condutor.model';
+import { CondutorService } from './../../../components/condutor/condutor.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LyTheme2, ThemeVariables } from '@alyle/ui';
+import { LyIconService } from '@alyle/ui/icon';
+
+const STYLES = (_theme: ThemeVariables) => ({
+  icon: {
+    marginAfter: '.5em'
+  }
+});
 
 @Component({
   selector: 'app-delete',
@@ -7,9 +18,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteCondutorComponent implements OnInit {
 
-  constructor() { }
+  readonly classes = this._theme.addStyleSheet(STYLES);
+
+  condutor: Condutor;
+
+  constructor(
+    private condutorService: CondutorService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private _theme: LyTheme2,
+    icon: LyIconService
+    ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.condutorService.readById(id).subscribe( condutor => {
+      this.condutor = condutor;
+    });
+  }
+
+  deleteCondutor(): void{
+    this.condutorService.delete(this.condutor.id).subscribe(() =>{
+      this.condutorService.showMessage('Condutor escluido!')
+      this.router.navigate(['/condutor'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/condutor']);
   }
 
 }
