819c6a8fde2ddb5a48f7bcab520aa6a528222fe9
silvioantonio <oliveira_0000@hotmail.com>
2020-05-23 10:54:43 -0300

    Create and Read components

--- src/app/views/condutor/condutor.component.ts
+++ src/app/views/condutor/condutor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-condutor',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CondutorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToCondutorCreate(): void {
+    this.router.navigate(['/condutor/create'])
   }
 
 }
--- src/app/views/condutor/read/read.component.ts
+++ src/app/views/condutor/read/read.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LyTheme2, ThemeVariables } from '@alyle/ui';
+import { LyIconService } from '@alyle/ui/icon';
+import { Condutor } from 'src/app/components/condutor/condutor.model';
+import { CondutorService } from 'src/app/components/condutor/condutor.service';
+
+const STYLES = (_theme: ThemeVariables) => ({
+  icon: {
+    marginAfter: '.5em'
+  }
+});
 
 @Component({
   selector: 'app-read-condutor',
@@ -7,9 +17,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadCondutorComponent implements OnInit {
 
-  constructor() { }
+  readonly classes = this._theme.addStyleSheet(STYLES);
+
+  condutores: Condutor[] = []
+  displayedColumns = [
+    'nome', 'cpf', 'matricula', 'numeroCNH', 'validade', 'categoriaCnh',
+    'cidade', 'bairro', 'complemento','numero','action'
+  ]
+
+  constructor(
+    private condutorService: CondutorService,
+    private _theme: LyTheme2,
+    icon: LyIconService) { }
 
   ngOnInit(): void {
+    this.condutorService.read().subscribe(res => {
+      this.condutores = res["content"]
+      console.log(res["content"])
+    })
   }
 
 }
